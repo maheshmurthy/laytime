5d8ff4513acaf5b80f6ac613ed6e25ce291c81a0
Mahesh Murthy <mahesh@mmurthy.com>
2009-09-13 00:31:35 -0600

    Successfully store the values in session for cpdetails and portdetails

--- app/controllers/laytime_controller.rb
+++ app/controllers/laytime_controller.rb
@@ -4,28 +4,47 @@ class LaytimeController < ApplicationController
   end
 
   def portdetails
-    session[:cp_detail] = CpDetail.new(params[:cp_detail])
-    if session[:cp_detail].invalid?
-        redirect_to :back
+    unless is_cpdetails_valid
+      redirect_to :action => 'cpdetails'
+      return
+    end
+
+    if session[:port_details]
+    # XXX Fix this block
+      @portdetails = session[:port_details]
+      logger.info @portdetails[0].errors.inspect
+      @portdetail = @portdetails[0]
     else
-      if session[:port_details]
-        @portdetails = session[:port_details]
-      else
-        @portdetails = Array.new
-        @portdetails << PortDetail.new
-        @portdetails << PortDetail.new
-      end
+      @portdetails = Array.new
+
+      portdetail = PortDetail.new
+      portdetail.operation = "loading"
+      @portdetails << portdetail
+
+      portdetail = PortDetail.new
+      portdetail.operation = "discharging"
+      @portdetails << portdetail
+
+      logger.info "***********************"
+      logger.info @portdetails[0].operation
+      logger.info @portdetails[1].operation
+      logger.info "***********************"
     end
   end
 
   def stfacts
-    session[:port_details] = PortDetail.new(params[:port_details])
+    unless is_portdetails_valid
+      redirect_to :action => 'portdetails'
+      return
+    end
+
     if session[:facts]
       @facts = session[:facts]
     else
       @facts = Array.new
       @facts <<  Fact.new
       @facts << Fact.new
+      # Need this for addRow method
       session[:facts] = @facts
     end
   end
@@ -40,10 +59,60 @@ class LaytimeController < ApplicationController
         logger.info "Saved!"
       else
         logger.info "Failed to save!"
-        logger.info @cpdetail.vessel
       end
     session[:cp_detail] = nil
     session[:port_details] = nil
     session[:facts] = nil
+  end
+
+  private 
+
+  def is_cpdetails_valid
+    unless(session[:cp_detail] && session[:cp_detail].errors && session[:cp_detail].errors.empty?)
+      # Take it from params if there is nothing in session 
+      # or there are errors in session
+      session[:cp_detail] = CpDetail.new(params[:cp_detail])
+    end
+
+    if session[:cp_detail].invalid?
+      # redirect to cp detail page instead of back
+      logger.info "Redirecting to cpdetail"
+      return false
+    end
+    
+    return true
+  end
+
+  def is_portdetails_valid
+    unless(session[:port_details] && session[:port_details][0].errors && session[:port_details][1].errors && session[:port_details][0].errors.empty? && session[:port_details][1].errors.empty?)
+      portdetails = Array.new
+      port_detail = PortDetail.new(params['portdetail'][0])
+      port_detail.calculation_type = params['calculation_type0']
+      port_detail.calculation_time_saved = params['calculation_time_saved0']
+      portdetails << port_detail
+
+      port_detail = PortDetail.new(params['portdetail'][1])
+      port_detail.calculation_type = params['calculation_type1']
+      port_detail.calculation_time_saved = params['calculation_time_saved1']
+      portdetails << port_detail
+      session[:port_details] = portdetails
+    end
+    validity0 = session[:port_details][0].invalid?
+    validity1 = session[:port_details][1].invalid?
+
+    # The reason I did it this way is because I want to call
+    # invalid method on both objects. If I do an or directly,
+    # if the first one satisfies, it wouldn't even go to the
+    # second object's invalid method
+    if validity0 || validity1
+      return false
+    end
+    #if session[:port_details][0].invalid?
+    #  return false
+    #end
+    #if session[:port_details][1].invalid?
+    #  return false
+    #end
+    return true
   end
 end
--- app/models/port_detail.rb
+++ app/models/port_detail.rb
@@ -1,5 +1,5 @@
 class PortDetail < ActiveRecord::Base
-   validates_presence_of :operation, :location, :cargo, :quantity, :description, :allowanceType, :allowance, :demurrage, :despatch, :calculation_type,:calculation_time_saved, :commission_pct, :pre_advise_date, :time_start, :time_end
+   validates_presence_of :operation, :location, :cargo, :quantity, :description, :allowanceType, :allowance, :demurrage, :despatch, :calculation_type,:calculation_time_saved, :commission_pct
    has_many :informative_entries
    has_many :time_infos
 end
