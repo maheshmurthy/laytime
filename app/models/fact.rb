5ae643810ce1f2d89f12ce23dd747116f408d613
Mahesh Murthy <mmurthy@mahesh-murthys-macbook-pro.local>
2009-11-01 01:34:52 -0600

    Changed format to custom

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -1,4 +1,21 @@
 class Fact < ActiveRecord::Base
   validates_presence_of :from, :to, :timeToCount, :val
   belongs_to :port_detail
+
+   def from_string
+     from.to_s(:custom) if from
+   end
+
+   def from_string=(from_str)
+     self.from = Time.parse(from_str) if from_str
+   end
+
+   def to_string
+     to.to_s(:custom) if to
+   end
+
+   def to_string=(to_str)
+     self.to = Time.parse(to_str) if to_str
+   end
+
 end
