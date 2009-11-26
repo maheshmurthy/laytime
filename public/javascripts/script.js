06fde4800c3b78d606c26da2574644fd52fe6067
Mahesh Murthy <mahesh@mmurthy.com>
2009-11-26 09:00:07 -0700

    Autofill sof from by taking from previous sof to

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,6 +2,18 @@ function addrow(operation) {
      var operationType = document.getElementById(operation);
      var set = operationType.getElementsByClassName('sof')[0];
 
+     var length = set.getElementsByClassName('row').length;
+
+    /* 
+      To of this sof is the from of next. 
+      Get the to of the last sof and populate the from of new sof
+     */
+
+     var rows = set.getElementsByClassName('row');
+     var lastRow = rows[rows.length - 1];
+     var to_date = lastRow.getElementsByClassName('time-info-text-date')[1].value;
+     var to_time = lastRow.getElementsByClassName('time-info-text-time')[1].value;
+
      var row = document.createElement('div')
      row.setAttribute('class','row');
 
@@ -9,24 +21,24 @@ function addrow(operation) {
      input.setAttribute('class','time-info-text-date');
      input.setAttribute('type','text');
      input.setAttribute('name',operation+'[][from_date]');
-     input.setAttribute('id','from_date');
-     input.setAttribute('value',"dd-mm-yy");
+     input.setAttribute('id','from_date_'+operation+'_'+length);
+     input.setAttribute('value',to_date);
      input.setAttribute('onfocus',"if(this.getValue() == 'hh:mm' || this.getValue() == 'dd-mm-yy') {this.clear();}");
      row.appendChild(input);
 
      input = document.createElement('input')
      input.setAttribute('class','time-info-text-time');
      input.setAttribute('type','text');
-     input.setAttribute('id','from_time');
+     input.setAttribute('id','from_time_'+operation+'_'+length);
      input.setAttribute('name',operation+'[][from_time]');
-     input.setAttribute('value',"hh:mm");
+     input.setAttribute('value',to_time);
      input.setAttribute('onfocus',"if(this.getValue() == 'hh:mm' || this.getValue() == 'dd-mm-yy') {this.clear();}");
      row.appendChild(input);
 
      input = document.createElement('input')
      input.setAttribute('class','time-info-text-date');
      input.setAttribute('type','text');
-     input.setAttribute('id','to_date');
+     input.setAttribute('id','to_date_'+operation+'_'+length);
      input.setAttribute('name',operation+'[][to_date]');
      input.setAttribute('value',"dd-mm-yy");
      input.setAttribute('onfocus',"if(this.getValue() == 'hh:mm' || this.getValue() == 'dd-mm-yy') {this.clear();}");
@@ -35,7 +47,7 @@ function addrow(operation) {
      input = document.createElement('input')
      input.setAttribute('class','time-info-text-time');
      input.setAttribute('type','text');
-     input.setAttribute('id','to_time');
+     input.setAttribute('id','to_time_'+operation+'_'+length);
      input.setAttribute('name',operation+'[][to_time]');
      input.setAttribute('value',"hh:mm");
      input.setAttribute('onfocus',"if(this.getValue() == 'hh:mm' || this.getValue() == 'dd-mm-yy') {this.clear();}");
