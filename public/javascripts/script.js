e6138fc9debbfabde490858ac45d73c21e2a4fe6
Mahesh Murthy <mahesh@mmurthy.com>
2009-11-27 00:09:58 -0700

    Added validations for SOF

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,15 +4,110 @@ function addrow(operation) {
 
      var length = set.getElementsByClassName('row').length;
 
-    /* 
+     var rows = set.getElementsByClassName('row');
+     var lastRow = rows[rows.length - 1];
+
+     var to_date = lastRow.getElementsByClassName('time-info-text-date')[1].value;
+     var to_time = lastRow.getElementsByClassName('time-info-text-time')[1].value;
+     var to = Date.parseExact(to_date, "dd-mm-yy");
+     if(to == null) {
+       alert('Please enter the date in format dd-mm-yy for to');
+       return false;
+     }
+     var hr = parseInt(to_time.split(':')[0]);
+     var min = parseInt(to_time.split(':')[1]);
+     if(!hr || !min) {
+       alert('Please enter the to time in format hh:mm');
+       return false;
+     }
+     to.set({hour: hr, minute: min});
+
+     var from_date = lastRow.getElementsByClassName('time-info-text-date')[0].value;
+     var from_time = lastRow.getElementsByClassName('time-info-text-time')[0].value;
+     var from = Date.parseExact(from_date, "dd-mm-yy");
+     if(from == null) {
+       alert('Please enter the date in format dd-mm-yy for from');
+       return false;
+     }
+
+     var hr = parseInt(from_time.split(':')[0]);
+     var min = parseInt(from_time.split(':')[1]);
+
+     if(!hr || !min) {
+       alert('Please enter the from time in format hh:mm');
+       return false;
+     }
+
+     from.set({hour: hr, minute: min});
+
+     if(from.compareTo(to) == 1) {
+       alert('From can not be greater that to. Please correct and try again');
+       return false;
+     }
+
+     var commence_date = document.getElementById('port_detail_time_start_date_' + operation).value;
+     var commence_time = document.getElementById('port_detail_time_start_time_' + operation).value;
+     var commence = Date.parseExact(commence_date, "dd-mm-yy");
+
+     if(commence == null) {
+       alert('Please enter the Laytime Commence date in format dd-mm-yy');
+       return false;
+     }
+
+     var hr = parseInt(commence_time.split(':')[0]);
+     var min = parseInt(commence_time.split(':')[1]);
+
+     if(!hr || !min) {
+       alert('Please enter the laytime commence time in format hh:mm');
+       return false;
+     }
+     commence.set({hour: hr, minute: min});
+
+     var complete_date = document.getElementById('port_detail_time_end_date_' + operation).value;
+     var complete_time = document.getElementById('port_detail_time_end_time_' + operation).value;
+     var complete = Date.parseExact(complete_date, "dd-mm-yy");
+
+     if(complete == null) {
+       alert('Please enter the Laytime complete date in format dd-mm-yy');
+       return false;
+     }
+
+     var hr = parseInt(complete_time.split(':')[0]);
+     var min = parseInt(complete_time.split(':')[1]);
+
+     if(!hr || !min) {
+       alert('Please enter the laytime complete time in format hh:mm');
+       return false;
+     }
+     complete.set({hour: hr, minute: min});
+
+     /* from and to datetime should be greater than equal commence date and 
+        less than equal complete date */
+     
+     if(from.compareTo(commence) < 0) {
+       alert('From can not be less than laytime commence date');
+       return false;
+     }
+
+     if(from.compareTo(complete) >= 0) {
+       alert('From can not be greater than laytime complete date');
+       return false;
+     }
+
+     if(to.compareTo(commence) <= 0) {
+       alert('To can not be less than laytime commence date');
+       return false;
+     }
+
+     if(to.compareTo(complete) > 0) {
+       alert('To can not be greater than laytime complete date');
+       return false;
+     }
+
+     /* 
       To of this sof is the from of next. 
       Get the to of the last sof and populate the from of new sof
      */
-
-     var rows = set.getElementsByClassName('row');
-     var lastRow = rows[rows.length - 1];
-     var to_date = lastRow.getElementsByClassName('time-info-text-date')[1].value;
-     var to_time = lastRow.getElementsByClassName('time-info-text-time')[1].value;
 
      var row = document.createElement('div')
      row.setAttribute('class','row');
