2b1fe7e4032d27733785c98669b168200a5ee6d7
Mahesh Murthy <mahesh@mmurthy.com>
2009-12-13 10:27:45 -0700

    Fixed a bug where portdetails used to blow up if fact_date was invalid.

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -57,8 +57,6 @@ class Fact < ActiveRecord::Base
    def from_date_day_string
      if from
        from.strftime("%a")
-     elsif from_date
-       from_date.strftime("%a")
      else
        "---"
      end
@@ -67,8 +65,6 @@ class Fact < ActiveRecord::Base
    def to_date_day_string
      if to
        to.strftime("%a")
-     elsif to_date
-       to_date.strftime("%a")
      else
        "---"
      end
